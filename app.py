7b966438e296b476cff7e52c4ccdae36c79c3d82
Md Sanowar Hossain <52736275+hossain-sanowar@users.noreply.github.com>
2022-10-16 19:22:08 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,81 +1,77 @@
+'''
+Author: Md Sanowar Hossain
+Email: md.sanowar21@gmail.com
+Date: 2022-Oct-10
+'''
+
 import pickle
 import streamlit as st
 import numpy as np
 
 
-#load the pickle file
-model=pickle.load(open('artifacts/model.pkl','rb'))
-books_name=pickle.load(open('artifacts/book_names.pkl','rb'))
-final_rating=pickle.load(open('artifacts/final_rating.pkl','rb'))
-book_pivot=pickle.load(open('artifacts/book_pivot.pkl','rb'))
+st.header('Book Recommender System:')
+model = pickle.load(open('artifacts/model.pkl','rb'))
+book_names = pickle.load(open('artifacts/book_names.pkl','rb'))
+final_rating = pickle.load(open('artifacts/final_rating.pkl','rb'))
+book_pivot = pickle.load(open('artifacts/book_pivot.pkl','rb'))
 
-#create local web page
-st.header("Books Recommender System")
 
-#select the book list
-selected_books=st.selectbox(
-    "Please Choose Your Favorite Book",
-    books_name
-)
-
-#define poster list
-def fecth_poster(suggestion):
-    book_name=[]
-    ids_index=[]
-    poster_url=[]
+def fetch_poster(suggestion):
+    book_name = []
+    ids_index = []
+    poster_url = []
 
     for book_id in suggestion:
         book_name.append(book_pivot.index[book_id])
 
-    for name in book_name[0]:
-        ids=np.where(final_rating['title']==name)[0][0]
+    for name in book_name[0]: 
+        ids = np.where(final_rating['title'] == name)[0][0]
         ids_index.append(ids)
 
     for idx in ids_index:
-        url=final_rating.iloc[idx]['image_url']
+        url = final_rating.iloc[idx]['image_url']
         poster_url.append(url)
-    
+
     return poster_url
 
 
-#return book list and poster list
-def recommend_books(book_name):
-    book_list=[]
 
-    book_id=np.where(book_pivot.index==book_name)[0][0]
-    distance, suggestion=model.kneighbors(book_pivot.iloc[book_id,:].values.reshape(1,-1),n_neighbors=6)
+def recommend_book(book_name):
+    books_list = []
+    book_id = np.where(book_pivot.index == book_name)[0][0]
+    distance, suggestion = model.kneighbors(book_pivot.iloc[book_id,:].values.reshape(1,-1), n_neighbors=6 )
 
-    poster_url =fecth_poster(suggestion)
-
+    poster_url = fetch_poster(suggestion)
+    
     for i in range(len(suggestion)):
-        books=book_pivot.index[suggestion[i]]
-        for j in books:
-            book_list.append(j)
+            books = book_pivot.index[suggestion[i]]
+            for j in books:
+                books_list.append(j)
+    return books_list , poster_url       
 
-    return book_list, poster_url
 
 
-#create button for showing recommendation books list
+selected_books = st.selectbox(
+    "Type or select a book from the dropdown",
+    book_names
+)
+
 if st.button('Show Recommendation'):
-    recommendation_books, poster_url=recommend_books(selected_books)
-    col1, col2, col3, col4, col5=st.columns(5)
-
+    recommended_books,poster_url = recommend_book(selected_books)
+    col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
-        st.text(recommendation_books[1])
-        st.image(poster_url[1],width=150)
-
+        st.text(recommended_books[1])
+        st.image(poster_url[1])
     with col2:
-        st.text(recommendation_books[2])
-        st.image(poster_url[2],width=100) 
+        st.text(recommended_books[2])
+        st.image(poster_url[2])
 
     with col3:
-        st.text(recommendation_books[3])
-        st.image(poster_url[3],width=150)     
-
+        st.text(recommended_books[3])
+        st.image(poster_url[3])
     with col4:
-        st.text(recommendation_books[4])
-        st.image(poster_url[4],width=150)
-
+        st.text(recommended_books[4])
+        st.image(poster_url[4])
     with col5:
-        st.text(recommendation_books[5])
-        st.image(poster_url[5],width=150)
+        st.text(recommended_books[5])
+        st.image(poster_url[5])
